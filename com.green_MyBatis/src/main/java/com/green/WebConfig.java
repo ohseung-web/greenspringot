bca52431b0f403e5fe39a73032c3b4f6913cf476
ohseung-web <kjb1045@naver.com>
2026-03-03 10:02:07 +0900

    2026-03-03update02

--- com.green_MyBatis/src/main/java/com/green/WebConfig.java
+++ com.green_MyBatis/src/main/java/com/green/WebConfig.java
@@ -18,17 +18,20 @@ public class WebConfig implements WebMvcConfigurer{
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
  
        // React public 폴더 경로 (본인 프로젝트 경로에 맞게 수정)
-        registry.addResourceHandler("/img/**")
-                .addResourceLocations("classpath:/static/img/","file:///C:/springPjt/greenspringot/com.green_MyBatis/frontend/public/img/");
-  
+       // registry.addResourceHandler("/img/**")
+       //         .addResourceLocations("classpath:/static/img/","file:///C:/springPjt/greenspringot/com.green_MyBatis/frontend/public/img/");
+   
+		   registry.addResourceHandler("/img/**")
+		    .addResourceLocations("classpath:/static/img/");
+		
     }
 	
 	// F5누르면 오류뜨는 이슈때문에 반드시 작성한다.
-	@Override 
-	public void addViewControllers(ViewControllerRegistry registry) { 
-		// 모든 경로를 index.html로 포워딩하여 React Router가 처리하게 함 	
-		registry.addViewController("/{path:[^\\.]*}") 
-		.setViewName("forward:/index.html");
-	} 
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+	// 모든 경로를 index.html로 포워딩 (확장자가 없는 경로 대상)
+	registry.addViewController("/{path:[^\\\\.]*}")
+	.setViewName("forward:/index.html");
+	}
 	
 }
